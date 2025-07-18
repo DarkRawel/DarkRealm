fdcd4ce1f9dfc1a30ed054c8ac3bdc0b1336e89f
DarkRawel <darkravel56@gmail.com>
2025-07-18 19:30:59 +0200

    update enemies.py

--- assets/enemies.py
+++ assets/enemies.py
@@ -1,2 +1,9 @@
 class Enemies:
-    pass
+    def __init__(self, name, health):
+        self.name = name
+        self.health = self.set_enemies_health(name)
+
+        def set_enemies_health(self):
+            enemies_health = {
+                "skeleton": 100,
+            }
