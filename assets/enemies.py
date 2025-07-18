fa402ec6c9a7efc7999d0cd57f5f20e387c2f863
DarkRawel <darkravel56@gmail.com>
2025-07-18 14:02:05 +0200

    updates to player.py and beginning for enemies

--- assets/enemies.py
+++ assets/enemies.py
@@ -1,1 +1,2 @@
-# this is where enemies will be managed.
+class Enemies:
+    pass
--- assets/player.py
+++ assets/player.py
@@ -4,11 +4,21 @@ class Player:
     def __init__(self, name, player_class):
         self.name = name
         self.player_class = player_class
-        self.health = 100
+        self.health = self.set_class_health(player_class)
         self.gold = 0
         self.level = 1
         self.experience = 0
         self.inventory = Inventory()
+
+    def set_class_health(self, player_class):
+        class_health = {
+            'wizard': 90,
+            'fighter': 120,
+            'rogue': 100,
+            'Bard': 90
+
+        }
+        return class_health.get(player_class.lower(), 100)
 
     def take_damage(self, amount):
         self.health = max(self.health - amount, 0)
