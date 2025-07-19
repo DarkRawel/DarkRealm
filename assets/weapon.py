2d6161fb73561a8a29da3c054880ca4c26136750
DarkRawel <darkravel56@gmail.com>
2025-07-19 19:44:45 +0200

    update

--- assets/weapon.py
+++ assets/weapon.py
@@ -5,21 +5,45 @@ class Weapon:
         self.name = name
     
     def damage(self):
-        player_weapon = {
+        weapon = {
             "sword": {
-                "short_sword": 3 + self.level,
-                "long_sword": 7 + self.level
+                "short_sword": {
+                    "stab": {"damage": 4 + self.level, "type": "piercing"},
+                    "slash": {"damage": 3 + self.level, "type": "slashing"}
+                },
+                "long_sword": {
+                    "stab": {"damage": 6 + self.level, "type": "piercing"},
+                    "slash": {"damage": 5 + self.level, "type": "slashing"}  
+                }
             },
             "staff": {
-                "broken_staff": 5 + self.level,
-                "wooden_staff": 8 + self.level
+                "broken_staff": {
+                    "burst_of_fire": {"damage": 4 + self.level, "type": "fire"},
+                    "lesser_heal": {"heal": 2 + self.level, "type": "health_regeneration"}
+                },
+                "wooden_staff": {
+                    "fire_ball": {"damage": 7 + self.level, "type": "fire"},
+                    "great_heal": {"heal": 4 + self.level, "type": "health_regeneration"}
+                }
             },
             "knife": {
-                "small_blade": 4 + self.level,
-                "medium_blade": 6 + self.level
+                "small_blade": {
+                    "stab": {"damage": 3 + self.level, "type": "piercing"},
+                    "trow": {"damage": 4 + self.level, "type": "piercing"}
+                },
+                "medium_blade":{
+                    "stab": {"damage": 3 + self.level, "type": "piercing"},
+                    "slash": {"damage": 4 + self.level, "type": "slashing"}
+                }
             },
             "instrument": {
-                "flute": 4 + self.level,
-                "guitar": 5 + self.level
-            }
+                "flute": {
+                    "strike": {"damage": 3 + self.level, "type": "bludgeon"},
+                    "play": {"damage_up": 1 + self.level, "type": "damage_up"}
+                },
+            "guitar": {
+                "strike": {"damage": 5 + self.level, "type": "bludgeon"},
+                "play": {"damage_up": 3 + self.level, "type": "damage_up"}
+                }
+            },
         }
