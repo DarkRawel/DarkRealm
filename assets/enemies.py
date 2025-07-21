c45170148d99bdf190b6e0086ab8976774ba8268
DarkRawel <darkravel56@gmail.com>
2025-07-21 18:42:09 +0200

    updated enemies.py

--- assets/enemies.py
+++ assets/enemies.py
@@ -5,6 +5,7 @@ from player import Player
 class Enemies:
     def __init__(self, name, e_level):
         self.name = name
+        self.e_level = e_level
         self.health = self.set_enemies_health(name)
         self.e_level = e_level + self.level
 
@@ -13,7 +14,7 @@ class Enemies:
             "skeleton": 17 + random.randint(1, 16) + self.e_level,
             "zombie": 22 + random.randint(1, 24) + self.e_level,
             "goblin": 9 + random.randint(1, 12) + self.e_level,
-            "dragon": 150 + random.randint(1, 25) + self.e_level
+            "dragon": 150 + random.randint(15, 25) + self.e_level
         }
         return enemies_health.get(name, 10)
     
@@ -25,12 +26,42 @@ class Enemies:
 
     def set_attack_power(self, name):
         attack_power = {
-            "skeleton": 5 + random.randint(1, 4) + self.e_level,
-            "zombie": 7 + random.randint(1, 6) + self.e_level,
-            "goblin": 4 + random.randint(1, 3) + self.e_level,
-            "dragon": 20 + random.randint(5, 10) + self.e_level
+            "skeleton": {
+                "bow": {
+                    "arrow_shot": {"damage": 5 + random.randint(1, 4) + self.e_level, "type": "piercing"}
+                },
+                "sword": {
+                    "slash": {"damage": 7 + random.randint(1, 2) + self.e_level, "type": "slashing"}
+                }
+            },
+            "zombie": {
+                "unarmed": {
+                    "claw": {"damage": 7 + random.randint(1, 6) + self.e_level, "type": "slashing"}
+                },
+                "sword": {
+                    "slash": {"damage": 8 + random.randint(1, 3) + self.e_level, "type": "slashing"}
+                }
+            },
+            "goblin": {
+                "small_blade": {
+                    "slash": {"damage": 4 + random.randint(1, 3) + self.e_level, "type": "slashing"}
+                },
+                "mace": {
+                    "strike": {"damage": 5 + random.randint(1, 3) + self.e_level, "type": "bludgeon"}
+                }
+            },
+            "dragon": {
+                "magic": {
+                    "fire_breath": {"damage": 20 + random.randint(5, 10) + self.e_level, "type": "fire"},
+                    "ice_breath": {"damage": 20 + random.randint(5, 10) + self.e_level, "type": "ice"}
+                },
+                "fyzical": {
+                    "claw": {"damage": 20 + random.randint(5, 10) + self.e_level, "type": "slashing"},
+                    "roar": {"damage": 5 + random.randint(1, 5) + self.e_level, "type": "debuff"}
+                }
+            }
         }
-        return attack_power.get(name, 2)
+        return attack_power.get(name, {})
 
     @property
     def is_alive(self):
